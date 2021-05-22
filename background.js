48d1ef0539e5fa9aa07811768ed6758b96901814
nisarg0 <nisarggogate212@gmail.com>
2021-05-22 12:24:31 +0530

    :lock: Added base-64 encryption and url checker

--- background.js
+++ background.js
@@ -1,35 +1,32 @@
 console.log("background is running...");
 
+// Data is encrypted inside this object
 var secretMessage;
 
-chrome.runtime.onMessageExternal.addListener(function (
-	Message,
-	sender,
-	sendResponse
-) {
-	console.log("Message recieved");
-	var action_url = Message.site;
+// Listens for even from casdashboard website
+chrome.runtime.onMessageExternal.addListener(
+	(Message, sender, sendResponse) => {
+		console.log("Message recieved");
+		secretMessage = Message;
 
-	secretMessage = Message;
+		// Open new tab with given url/
+		chrome.tabs.create({ url: Message.url });
+		console.log("new tab opened");
 
-	// Open new tab with given url/
-	chrome.tabs.create({ url: action_url });
-	console.log("new tab opened");
+		sendResponse({ success: "success" });
+	}
+);
 
-	sendResponse({ success: "success" });
-});
-
-chrome.runtime.onMessage.addListener(async function (
-	message,
-	sender,
-	sendResponse
-) {
+// Message passed with website url
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	// If current tab url is the one which has to signin form to fill.
-	if (message.data === secretMessage.site) {
+	if (message.data === secretMessage.url) {
 		// console.log("msg sent from background");
 		chrome.runtime.sendMessage({ data: secretMessage }, (Response) => {
 			console.log(Response);
 		});
+	} else {
+		console.log("wrong tab");
 	}
 	sendResponse({ success: "Request received" });
 });
--- popup.js
+++ popup.js
@@ -1,26 +1,26 @@
-// Send currentTab url to background script
+// Send currentTab url to background script when user clicks on extension
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 	var currentTab = tabs[0];
 	chrome.runtime.sendMessage({ data: currentTab.url });
 	console.log("Url for current tab : " + currentTab.url);
 });
 
-// If verification is successful then background will send the data to poopup
-chrome.runtime.onMessage.addListener(async function (
-	message,
-	sender,
-	sendResponse
-) {
+// Function to fill only if on correct website
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 	data = message.data;
 
 	// Fill username field
-	await chrome.tabs.executeScript(null, {
-		code: `document.getElementById('${data.userNameFieldName}').value = '${data.username}'`,
+	chrome.tabs.executeScript(null, {
+		code: `document.getElementById('${
+			data.userNameFieldName
+		}').value = '${atob(data.username)}'`,
 	});
 
 	// Fill password field
-	await chrome.tabs.executeScript(null, {
-		code: `document.getElementById('${data.passwordFieldName}').value = '${data.password}'`,
+	chrome.tabs.executeScript(null, {
+		code: `document.getElementById('${
+			data.passwordFieldName
+		}').value = '${atob(data.password)}'`,
 	});
 
 	sendResponse({ success: "success" });
