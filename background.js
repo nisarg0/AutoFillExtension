a20b3f63c13ddad4ba4f07ca95703ebbca219fba
nisarg0 <nisarggogate212@gmail.com>
2021-05-27 11:43:59 +0530

    :books: added 2 levels of encryption

--- background.js
+++ background.js
@@ -1,29 +1,34 @@
 console.log("background is running...");
-
 // Data is encrypted inside this object
 var encodedMessage;
 
 // Listens for even from casdashboard website
 chrome.runtime.onMessageExternal.addListener(
-	(Message, sender, sendResponse) => {
-		console.log("Message recieved");
-		encodedMessage = Message;
-		console.log(Message);
-		// Open new tab with given url/
-		var decodedMsg = atob(Message);
-		var secretMsg = JSON.parse(decodedMsg);
+	async (Message, sender, sendResponse) => {
+		console.log("0 Message recieved");
+		console.log("1" + Message);
 
-		chrome.tabs.create({ url: secretMsg.url });
-		console.log("new tab opened");
+		encodedMessage = Message;
+		decodedMsg = await decryptLevel1(Message);
+
+		chrome.tabs.create({ url: decodedMsg.url });
+		console.log("3 new tab opened");
 
 		sendResponse({ success: "success" });
 	}
 );
 
+async function decryptLevel1(Msg) {
+	var decoded = atob(Msg);
+	return await JSON.parse(decoded);
+}
+
 // Message passed with website url
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	// If current tab url is the one which has to signin form to fill.
+
 	var decodedMsg = atob(encodedMessage);
+
 	var secretMsg = JSON.parse(decodedMsg);
 	if (message.data === secretMsg.url) {
 		// console.log("msg sent from background");
--- popup.js
+++ popup.js
@@ -7,17 +7,90 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
 // Function to fill only if on correct website
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-	data = message.data;
+	var data = message.data;
+
+	// Level2 decryption
+	var decodedusername = decrypt(data.username, "1234");
+	var decodedpass = decrypt(data.password, "1234");
 
 	// Fill username field
 	chrome.tabs.executeScript(null, {
-		code: `document.getElementById('${data.userNameFieldName}').value = '${data.username}'`,
+		code: `document.getElementById('${data.userNameFieldName}').value = '${decodedusername}'`,
 	});
 
 	// Fill password field
 	chrome.tabs.executeScript(null, {
-		code: `document.getElementById('${data.passwordFieldName}').value = '${data.password}'`,
+		code: `document.getElementById('${data.passwordFieldName}').value = '${decodedpass}'`,
 	});
 
 	sendResponse({ success: "success" });
 });
+
+// ------------------------Wheeler & Needham’s Tiny Decryptio Algorithm--------------------
+
+// use (16 chars of) 'password' to decrypt 'ciphertext' with xTEA
+function decrypt(ciphertext, password) {
+	var v = new Array(2),
+		k = new Array(4),
+		s = "",
+		i;
+
+	for (var i = 0; i < 4; i++)
+		k[i] = Str4ToLong(password.slice(i * 4, (i + 1) * 4));
+
+	ciphertext = unescCtrlCh(ciphertext);
+	for (i = 0; i < ciphertext.length; i += 8) {
+		// decode ciphertext into s in 64-bit (8 char) blocks
+		v[0] = Str4ToLong(ciphertext.slice(i, i + 4));
+		v[1] = Str4ToLong(ciphertext.slice(i + 4, i + 8));
+		decode(v, k);
+		s += LongToStr4(v[0]) + LongToStr4(v[1]);
+	}
+
+	// strip trailing null chars resulting from filling 4-char blocks:
+	s = s.replace(/\0+$/, "");
+
+	return decodeURI(s);
+}
+
+function decode(v, k) {
+	var y = v[0],
+		z = v[1];
+	var delta = 0x9e3779b9,
+		sum = delta * 32;
+
+	while (sum != 0) {
+		z -= (((y << 4) ^ (y >>> 5)) + y) ^ (sum + k[(sum >>> 11) & 3]);
+		sum -= delta;
+		y -= (((z << 4) ^ (z >>> 5)) + z) ^ (sum + k[sum & 3]);
+	}
+	v[0] = y;
+	v[1] = z;
+}
+
+// supporting functions
+
+function Str4ToLong(s) {
+	// convert 4 chars of s to a numeric long
+	var v = 0;
+	for (var i = 0; i < 4; i++) v |= s.charCodeAt(i) << (i * 8);
+	return isNaN(v) ? 0 : v;
+}
+
+function LongToStr4(v) {
+	// convert a numeric long to 4 char string
+	var s = String.fromCharCode(
+		v & 0xff,
+		(v >> 8) & 0xff,
+		(v >> 16) & 0xff,
+		(v >> 24) & 0xff
+	);
+	return s;
+}
+
+function unescCtrlCh(str) {
+	// unescape potentially problematic nulls and control characters
+	return str.replace(/!\d\d?\d?!/g, function (c) {
+		return String.fromCharCode(c.slice(1, -1));
+	});
+}
